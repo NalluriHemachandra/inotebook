7b7213adc0d05cf5f5f20a46a0d8430c4f9ccdb1
Hema Chandra Gupta Nalluri <nallurichandu546@gmail.com>
2022-08-31 22:22:29 +0530

    Added code to connected to mongoDb.

---
+++ backend/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const mongooseUri = "mongodb://localhost:27017/?directConnection=true&tls=false&readPreference=primary";
+
+const connectToMongo = () =>{
+    mongoose.connect(mongooseUri, () => {
+        console.log("Connected to Mongoos DB Successfully");
+    })
+}
+
+module.exports = connectToMongo;
