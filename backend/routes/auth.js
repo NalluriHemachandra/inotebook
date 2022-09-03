42bd07c782f7b87d32ef4e2471f583728c7cbffc
Hema Chandra Gupta Nalluri <nallurichandu546@gmail.com>
2022-09-03 16:42:37 +0530

    Added middleware to send the request to the API and also added post request to save the user details

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User')
 
-router.get('/',(req,res) => {
-    obj = {
-        a : 'thois',
-        number : 7
-    }
-    res.json(obj);
+//Create user using POST : "/api/auth/". Doesn't require auth
+router.post('/',(req,res) => {
+    console.log(req.body);
+    const user = User(req.body);
+    user.save();
+    res.send(req.body);
 })
 
 module.exports = router
